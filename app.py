c576e436334fcab8338f8d07f4ba23dcd10e9d12
A-Rehman0 <96643540+A-Rehman0@users.noreply.github.com>
2026-03-31 14:47:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -119,7 +119,7 @@ else:
 
 # ---------------- FOOTER ----------------
 st.markdown("---")
-col1, col2 = st.columns(2)
+col1, col2, col3 = st.columns([1, 1, 3])  # last column is just empty space
 
 with col1:
     st.link_button(
@@ -132,5 +132,6 @@ with col2:
         "📝 Mark Attendance",
         "YOUR_GOOGLE_FORM_LINK_HERE"
     )
+
 
 st.caption("After completing task, report to Team Leader.")
